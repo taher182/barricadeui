212d33133ac648f7ce0c453c5b89cb5fd840694b
taher182 <106422401+taher182@users.noreply.github.com>
2023-12-27 20:01:40 +0000

    ccc

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -14,7 +14,7 @@ class Footer extends React.Component{
         let year = date.getFullYear();
         return(
             <>
-            <div className='container-fluid  text-center bg-info fw-bold text-shadow  d-flex justify-content-center align-items-center' style={{position:"fixed", bottom:"0", left:"0", right:"0", zIndex:"1000"}}>
+            <div className='container-fluid  text-center bg-info fw-bold  d-flex justify-content-center align-items-center' style={{position:"fixed", bottom:"0", left:"0", right:"0", zIndex:"1000"}}>
                 <h6 className='mt-3 mb-3'>&copy; {year} Developed by Department of Electrical and Electronics Engineering</h6>
             </div>
             </>
